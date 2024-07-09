1e6bf0ae2fe4f2796985f4efc0b1fdd1f1c9d8e4
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-07-09 16:27:07 +0300

    Add page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import {Calendar, Layout, Typography} from "antd";
+import {Content, Footer, Header} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 function App() {
-  return (
-    <div className="App">
-      <h1>Initialize</h1>
-    </div>
-  );
+    return (
+        <Layout>
+            <Header className="header">
+                <Typography.Title level={2}>AirNet test assignment</Typography.Title>
+            </Header>
+            <Layout className="content_layout">
+                <Sider width="25%" className="sider">
+                    <Calendar fullscreen={false}/>
+                </Sider>
+                <Content className="content">
+                    <Calendar></Calendar>
+                </Content>
+            </Layout>
+            <Footer className="footer">To-do test assignment ©{new Date().getFullYear()}</Footer>
+        </Layout>
+    );
 }
 
 export default App;
